a0463e44054c8fed4e251617a89b5632df0a65c1
ihab abadi <ihab@utopios.net>
2021-04-26 20:13:53 +0200

    version 2

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -2,13 +2,26 @@ import React from "react"
 import { PersonComponent } from "./PersonComponent"
 
 export class FirstComponent extends React.PureComponent {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            persons : [
+                {firstName : 'toto', lastName : 'tata'},
+                {firstName : 'minet', lastName : 'titi'},
+            ]
+        }
+    }
     render() {
         return(
             <div>
                 <h1>Hello from our First component</h1>
-                <PersonComponent firstName ="ihab" lastName="abadi"></PersonComponent>
-                <PersonComponent firstName ="tata" lastName="toto"></PersonComponent>
+                {/* <PersonComponent person={{firstName : 'ihab', lastName : 'abadi'}} firstName ="ihab" lastName="abadi"></PersonComponent>
+                <PersonComponent firstName ="tata" lastName="toto"></PersonComponent> */}
+                {this.state.persons.map((element,index) => {
+                    return(
+                        <PersonComponent person={element}></PersonComponent>
+                    )
+                })}
             </div>            
         )
     }
--- src/PersonComponent.js
+++ src/PersonComponent.js
@@ -3,14 +3,30 @@ import React, { PureComponent } from 'react'
 export class PersonComponent extends PureComponent {
     constructor(props) {
         super(props);
+
     }
     render() { 
+        //destructeur
+        const {firstName, lastName, person} = this.props
         return (  
             <div>
-                <div>Nom : {this.props.lastName}</div>
-                <div>Prénom : {this.props.firstName}</div>
+                <div>Nom : {lastName} {"<=>"} {person != undefined ? person.lastName : null}</div>
+                <div>Prénom : {firstName} {"<=>"} {person != undefined ? person.firstName : null}</div>
             </div>
         );
     }
 }
- 
+
+
+// export const PersonComponent = (props) => {
+
+//     const { firstName, lastName, person } = props
+
+//     return (
+//         <div>
+//             <div>Nom : {lastName} {"<=>"} {person != undefined ? person.lastName : null}</div>
+//             <div>Prénom : {firstName} {"<=>"} {person != undefined ? person.firstName : null}</div>
+//         </div>
+//     )
+// }
+
